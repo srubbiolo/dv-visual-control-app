27d212daf380c2838f2d24e9bd080f3ee628e7f7
Santiago Rubbiolo <Santiago.rubbiolo@sprint.com>
2020-06-28 20:58:45 -0300

    fix React error con return(

--- src/components/DisplayComponent/Display.component.jsx
+++ src/components/DisplayComponent/Display.component.jsx
@@ -18,8 +18,9 @@ const DisplayComponent = ({ sources, thumb }) => {
     updateAllOptions();
   });
 
+  //Without the key it never refreshes otherwise only the son who is never born gets the update
+  //Another funny thing this comment under the return explodes on the prod build because of minify, probably babel error.
   return (
-    //Without the key it never refreshes otherwise only the son who is never born gets the update
     <video key={thumb} width="80%" height="80%" poster={thumb} ref={videoRef}>
       <source src={sources[0]} type="video/mp4" />
       Your browser does not support the video tag.
