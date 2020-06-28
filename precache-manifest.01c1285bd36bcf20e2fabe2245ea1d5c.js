5b02329377de4b70459ccd72d471cb13187b31bb
Santiago Rubbiolo <Santiago.rubbiolo@sprint.com>
2020-06-28 20:46:10 -0300

    Updates

---
+++ precache-manifest.01c1285bd36bcf20e2fabe2245ea1d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ed0b155575babfaee4485cfcc858ca",
+    "url": "/dv-visual-control-app/index.html"
+  },
+  {
+    "revision": "4d1bda88b9816f856b25",
+    "url": "/dv-visual-control-app/static/js/2.3a15afcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dv-visual-control-app/static/js/2.3a15afcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c11438673bc103bc245",
+    "url": "/dv-visual-control-app/static/js/main.96c311db.chunk.js"
+  },
+  {
+    "revision": "7cb87e2d2f41f3175586",
+    "url": "/dv-visual-control-app/static/js/runtime-main.2eb76539.js"
+  }
+]);
